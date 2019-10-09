a4a0eb4ac4ad626300dabcd92e066fb0ec28072d
dingzhengru <qws7869vdx@gmail.com>
2019-10-09 12:56:41 +0800

    updata firebase-init(my-firebase-function)

--- database-init.js
+++ database-init.js
@@ -1,13 +1,47 @@
-function initDatabase(db, coll, data){
+// 刪除Collection全部資料
+function clearCollection(db, coll) {
+    console.log('clearCollection', coll);
+    db.collection(coll).get().then((shotsnap) => {
+        shotsnap.forEach((doc) => {
+            index++;
+            db.collection(coll).doc(doc.id).delete();
+        });
+    });
+}
+
+// 新增資料進去(自動ID)
+function setNewData(db, coll, data) {
+    console.log('setNewData', coll, data.length);
     if(Array.isArray(data)) {
-        data.forEach(function(d, index) {
+        data.forEach(function(d) {
             db.collection(coll).doc().set(d);
         })
     } else {
         db.collection(coll).doc().set(data);
     }
-    
 }
+
+// 刪除Collection全部資料後再放進去
+function initCollection(db, coll, data) {
+    console.log('initCollection', coll, data.length);
+    let index = 0;
+    db.collection(coll).get().then((shotsnap) => {
+        if(shotsnap.docs.length == 0) {
+            console.log('沒資料 直接Set進去');
+            setNewData(db, coll, data);
+        } else {
+            console.log('先清光資料，再Set進去')
+            shotsnap.forEach((doc) => {
+                index++;
+                db.collection(coll).doc(doc.id).delete();
+                if(index == shotsnap.docs.length)
+                    setNewData(db, coll, data);
+            });
+        }
+        
+    }).catch(function(error) { console.log("initCollection Error:", error) });
+}
+
 
 var defaultProject = firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
@@ -31,4 +65,4 @@ var products = [
 ]
 
 
-// initDatabase(db, 'products', products);
+initCollection(db, 'products', products);
